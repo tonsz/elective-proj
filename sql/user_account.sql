ed48ad285f38bb034649bb69ca0b462c13d6d6ae
tonsz <tmestrera@gmail.com>
2022-07-06 17:00:42 +0800

    SQL update

--- sql/user_account.sql
+++ sql/user_account.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2022 at 06:12 PM
+-- Generation Time: Jul 06, 2022 at 11:00 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -66,16 +66,9 @@ CREATE TABLE `candidates_tbl` (
 --
 
 INSERT INTO `candidates_tbl` (`cand_id`, `cand_name`, `cand_age`, `cand_desc1`, `cand_desc2`, `cand_desc3`, `cand_image_path`, `cand_election`, `cand_votes`) VALUES
-(5, 'Calliope', 19, 'A', 'B', 'C', '../uploads/Calliope.jpg', 1999, 0),
+(5, 'Calliope', 19, 'A', 'B', 'C', '../uploads/Calliope.jpg', 1999, 1),
 (6, 'Jules', 16, 'H', 'J', 'K', '../uploads/Jules.jpg', 8955, 0),
-(9, 'Pikachu', 10, 'hello', 'yellow', 'volt', '../uploads/pikachu.jpg', 2578, 0),
-(10, 'Glaceon', 11, 'Blue', 'Pretty', 'cute', '../uploads/glacial.png', 2578, 0),
-(11, 'Jigglypuff', 0, 'Spirit animal/pokemon ni Jaz', 'Pink', 'Ball', '../uploads/jigglypuff.jpg', 2578, 0),
-(12, 'Snorlax', 15, 'Sleeping is life', 'Cutie', 'Mood', '../uploads/Snorlax.jpg', 2578, 0),
-(13, 'Cyndaquil', 19, 'Ang hirap ng name ah', 'Hirap din ng spelling', 'Mukha siyang sad awts', '../uploads/cyndaquil.png', 2578, 0),
-(14, 'Uncle Sam', 58, 'W5l4n6 Th1t1bHa6', 'Mh4LhUPh3T', 'N1C3', '../uploads/uncle.jpg', 1999, 0),
-(16, 'Pedro Calungsod', 17, 'holy', 'saint', 'dapat tularan', '../uploads/saint-pedro-calungsod-image.jpg', 1234, 0),
-(17, 'Lorenzo Ruiz', 42, 'Martyr', 'Saint', 'Amen', '../uploads/st lorenzo ruiz.jpg', 1234, 0);
+(7, 'Elino', 28, 'X', 'X', 'X', '../uploads/Elinor.jpg', 1999, 2);
 
 -- --------------------------------------------------------
 
@@ -117,7 +110,6 @@ CREATE TABLE `elections_tbl` (
 --
 
 INSERT INTO `elections_tbl` (`e_id`, `e_name`, `e_start`, `e_end`, `cand_count`, `e_owner`) VALUES
-(1234, 'Saints', '2022-07-02', '2022-07-03', 2, 2),
 (1999, 'ElectioNNN', '2022-06-29', '2022-07-06', 3, 3),
 (2578, 'Best Pokemon Names', '2022-06-28', '2022-06-30', 5, 3),
 (7777, 'Mushroom', '2022-06-29', '2022-07-06', 3, 3),
@@ -143,6 +135,28 @@ INSERT INTO `help_tbl` (`id`, `question`, `answer`) VALUES
 (1, 'How do I vote?', 'Dunno'),
 (2, 'How to manage your account?', 'Idk'),
 (3, 'How to manage your account?', 'I don\'t know');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `receipt_tbl`
+--
+
+CREATE TABLE `receipt_tbl` (
+  `e_id` int(11) NOT NULL,
+  `v_id` int(11) NOT NULL,
+  `c_id` int(11) NOT NULL,
+  `receipt_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `receipt_tbl`
+--
+
+INSERT INTO `receipt_tbl` (`e_id`, `v_id`, `c_id`, `receipt_id`) VALUES
+(1999, 4, 5, 3),
+(1999, 3, 7, 4),
+(1999, 1, 7, 5);
 
 -- --------------------------------------------------------
 
@@ -206,6 +220,15 @@ ALTER TABLE `help_tbl`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `receipt_tbl`
+--
+ALTER TABLE `receipt_tbl`
+  ADD PRIMARY KEY (`receipt_id`),
+  ADD KEY `e_id` (`e_id`),
+  ADD KEY `c_id` (`c_id`),
+  ADD KEY `v_id` (`v_id`);
+
+--
 -- Indexes for table `voter_tbl`
 --
 ALTER TABLE `voter_tbl`
@@ -226,13 +249,19 @@ ALTER TABLE `admin_tbl`
 -- AUTO_INCREMENT for table `candidates_tbl`
 --
 ALTER TABLE `candidates_tbl`
-  MODIFY `cand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `cand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `help_tbl`
 --
 ALTER TABLE `help_tbl`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `receipt_tbl`
+--
+ALTER TABLE `receipt_tbl`
+  MODIFY `receipt_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `voter_tbl`
@@ -255,6 +284,14 @@ ALTER TABLE `candidates_tbl`
 --
 ALTER TABLE `elections_tbl`
   ADD CONSTRAINT `election_owner` FOREIGN KEY (`e_owner`) REFERENCES `admin_tbl` (`id`);
+
+--
+-- Constraints for table `receipt_tbl`
+--
+ALTER TABLE `receipt_tbl`
+  ADD CONSTRAINT `c_id` FOREIGN KEY (`c_id`) REFERENCES `candidates_tbl` (`cand_id`),
+  ADD CONSTRAINT `e_id` FOREIGN KEY (`e_id`) REFERENCES `elections_tbl` (`e_id`),
+  ADD CONSTRAINT `v_id` FOREIGN KEY (`v_id`) REFERENCES `voter_tbl` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
