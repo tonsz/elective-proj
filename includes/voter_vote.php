f29a30331b6f1b280990fe1cdfe665d53c520457
hnnhlyd0824 <johnloydmalinay@gmail.com>
2022-07-08 08:02:36 +0800

    voter's page css

--- includes/voter_vote.php
+++ includes/voter_vote.php
@@ -9,9 +9,11 @@
 
   // check if vote_status is set from voting-page.php
   if (isset($_SESSION['vote_status'])) {
-    $msg = "You have already voted for this election";
+
+    $msg =' <span style= "color:gray;">You have already voted for this election </span>';
+
   }
-  
+
 
   // fetch election details
   $access = $conn->prepare("select * from elections_tbl where e_id = ?");
@@ -19,9 +21,9 @@
   $access->execute();
   $e_result = $access->get_result();
   // store election details in $elec
-  if ($e_result->num_rows > 0){     
+  if ($e_result->num_rows > 0){
     $elec = $e_result -> fetch_assoc();
-  } 
+  }
 
   // fetch candidate details
   $access = $conn->prepare("select * from candidates_tbl where cand_election = ?");
@@ -30,13 +32,13 @@
   $result = $access->get_result();
 // there are multiple rows, so the records are to be stored in an array
 // accessed via foreach loop
-  $details = array(); 
+  $details = array();
   while ($row = $result->fetch_array()){
     $details[] = $row; // storing every row in details array
 
 
 
-    // if the any of the vote buttons are clicked 
+    // if the any of the vote buttons are clicked
     // each one of them is identifiable by original cand_id in name='' attribute
     if (isset($_POST[$row['cand_id']])){
       $e_id = $row['cand_election'];
@@ -47,7 +49,7 @@
         $store = $conn->prepare("insert into receipt_tbl (e_id, v_id, c_id) values(?,?,?)");
         $store->bind_param("iii", $e_id, $v_id, $c_id);
         $store->execute();
-        $success = "You have successfully casted your vote!";
+        $success = ' <span style= "color:green;">You have successfully casted your vote! </span>';
         $error ="";
 
         // update candidate's number of votes
@@ -57,9 +59,9 @@
         $access->execute();
         $res = $access->get_result();
         $access->close();
-      
+
     }
-        
+
     }
 
 
@@ -71,14 +73,15 @@
 
 <!-- Cancel Button, to be improved.
 If possible, unset $_SESSION['e_id'] when this is clicked -->
-  <button onclick="window.location.href='voting-page.php'" class="cancel-btn">CANCEL</button>
-    
-    
+  <button onclick="window.location.href='voting-page.php'" class="cancel-btn">Back</button>
+
+
   <!-- Display election details -->
     <div class = "election-title">
     <?php echo $elec['e_name'] . " (ID: ";?>
-    <?php echo "<input value='" . $elec['e_id'] . "' name ='".$elec['e_id'] . "' class = 'election-title' type = 'number' />";?></div>
- 
+    <?php echo $elec['e_id'] . ")" ;?>
+  </div>
+
 
 
 
@@ -90,22 +93,23 @@ If possible, unset $_SESSION['e_id'] when this is clicked -->
 
 
   <!-- Display candidates -->
-  <form action="voter_vote.php" method = "post">
-    <table>
-      <tr> 
+  <center><form action="voter_vote.php" method = "post">
+    <table class>
+      <tr>
         <!-- Displaying info -->
         <td class = "e-subtitle"><i>Name</i></td>
         <td class = "next-subtitle"><i>Information</i></td>
       </tr>
 
+
       <table class>
         <?php foreach ($details as $det): ?>
         <tr>
-          <td class = "back"><?php echo "<img height=130 width=130 src='".$det['cand_image_path']."' >";?></td>
-          <td class = "back">
-            <p class = "text"><?php echo $det['cand_name'];?></p><br>
+          <td class = "back1"><?php echo "<img height=250 width=250 src='".$det['cand_image_path']."' >";?></td>
+          <td class = "back2">
+            <p class = "text"><?php echo  $det['cand_name'];?></p><br>
           </td>
-          <td class = "back">
+          <td class = "back3">
             <ul class = "textlist">
               <li><?php echo $det['cand_desc1'];?></li>
 						  <li><?php echo $det['cand_desc2'];?></li>
@@ -113,24 +117,27 @@ If possible, unset $_SESSION['e_id'] when this is clicked -->
 						  <li><?php echo $det['cand_age'] . " years old";?></li>
             </ul>
           </td>
-          <td class = "back">
-            
+          <td class = "back4">
+
           <!-- Display buttons for each candidate -->
-               <?php 
+               <?php
                 // check if vote_status is set or voter had already voted
                if (isset($_SESSION['vote_status'])) {
                 // disabled buttons if yes, pls change class/color for design
-                echo "<input value='vote' name='".$det['cand_id'] ."'class = 'voteButton' type='submit' disabled />";
-                
+                echo "<input value='Voted' name='".$det['cand_id'] ."'class = 'voteButton' type='submit' disabled />";
+
                } else {
-                // working button if not yet set 
-                echo "<input value='vote' name='".$det['cand_id'] ."'class = 'voteButton' type='submit' />";
+                // working button if not yet set
+                echo "<input value='Vote' name='".$det['cand_id'] ."'class = 'voteButton' type='submit' />" ;
+
                }
-               
+
                ?>
 
              </form>
+           </center>
           </td>
+
         </tr>
       <?php endforeach ?>
     </table>
